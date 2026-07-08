3bd1baa3b953616347688f9338c01a96859251ff
Diganta <digantamodak1234@gmail.com>
2026-07-09 00:10:08 +0530

    home.ejs is initiaized with demo data

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const mongoose = require("mongoose")
 const path = require("path")
 const ejsMate =  require("ejs-mate")
+const Service = require("./models/service")
 
 //app config
 app.engine("ejs",ejsMate)
@@ -11,6 +12,18 @@ app.set("views",path.join(__dirname,"views"))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname,"public")))
 
+//database-connections
+main()
+.then(()=>{
+    console.log("Connected to monogoDB")
+}).catch((err)=>{
+    console.log(`error occured \n ${err}`)
+})
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/glamora")
+    
+}
 
 const port = 3000
 
@@ -18,8 +31,10 @@ app.get("/",(req,res)=>{
     res.send("It's Working...")
 })
 
-app.get("/home",(req,res)=>{
-    res.render("home.ejs")
+app.get("/home",async (req,res)=>{
+    let allServices = await Service.find({})
+    res.render("home.ejs",{allServices})
+    
 })
 
 app.listen(port,()=>{
