72865b5d248d3ccec077799e65ca5a6eaae2fd55
Diganta <digantamodak1234@gmail.com>
2026-07-09 20:58:33 +0530

    files inside services folder are being created. edit.ejs and new.ejs is created, functionality is successfully implemented.

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const app = express()
 const mongoose = require("mongoose")
 const path = require("path")
 const ejsMate =  require("ejs-mate")
+const methodOverride = require("method-override")
 const Service = require("./models/service")
+
+
 
 //app config
 app.engine("ejs",ejsMate)
@@ -11,6 +14,7 @@ app.set("view engine","ejs")
 app.set("views",path.join(__dirname,"views"))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname,"public")))
+app.use(methodOverride("_method"))
 
 //database-connections
 main()
@@ -46,6 +50,12 @@ app.get("/services/new",(req,res)=>{
     res.render("services/new.ejs")
 })
 
+app.get("/services/:id/edit",async (req,res)=>{
+    const {id} = req.params
+    const service = await Service.findById(id)
+    res.render("services/edit.ejs",{service})
+})
+
 app.get("/services/:id",async(req,res)=>{
     const {id} = req.params
     const service = await Service.findById(id)
@@ -53,10 +63,23 @@ app.get("/services/:id",async(req,res)=>{
 
 })
 
-app.get("/services/:id/edit",async (req,res)=>{
+app.post("/services", async (req,res)=>{
+    const newService = new Service(req.body)
+    await newService.save()
+    res.redirect("/services")
+})
+
+app.put("/services/:id",async(req,res)=>{
     const {id} = req.params
-    const service = await Service.findById(id)
-    res.render("services/edit.ejs",{service})
+    await Service.findByIdAndUpdate(id,req.body)
+    res.redirect(`/services/${id}`)
+})
+
+app.delete("/services/:id", async (req,res)=>{
+    const {id}= req.params
+    let deleted_service = await Service.findByIdAndDelete(id)
+    console.log(deleted_service)
+    res.redirect("/services")
 })
 
 
--- models/service.js
+++ models/service.js
@@ -40,8 +40,15 @@ const serviceSchema = new Schema({
     },
 
     image: {
-        url: String,
-        filename: String
+        url: {
+            type : String,
+            default : "https://plus.unsplash.com/premium_photo-1780358585402-8c11e746e38a?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            set : (url) => url ==""?"https://plus.unsplash.com/premium_photo-1780358585402-8c11e746e38a?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D":url
+        },
+        filename: {
+            type: String,
+            default : "Unknown_Image"
+        }
     },
 
     isAvailable: {
